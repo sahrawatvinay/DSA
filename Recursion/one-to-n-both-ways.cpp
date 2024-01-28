61b19ec472ef88685c1c99831c74b28e6b5d717f
vinay <vinaysahrawat183a@gmail.com>
2024-01-28 07:33:17 -0500

    tail recursion comment added

--- Recursion/one-to-n-both-ways.cpp
+++ Recursion/one-to-n-both-ways.cpp
@@ -7,6 +7,11 @@ void print1toN(int n)
     print1toN(n - 1);
     cout << n << " ";
 }
+/// @brief prints numbers from n to 1
+/// Tail recursion : A recrsive fn is tail recursive when parent fn has nothing more to do when child fn has finished.
+/// Why is it faster than normal rec fn : beacuse it does not have to maintain state, takes less time and less auxillary space for execution
+/// Can we convert every non tail recursive sln to tail recursive sln : NO
+/// @param n nth value
 void printNto1(int n)
 {
     if (n == 0)
