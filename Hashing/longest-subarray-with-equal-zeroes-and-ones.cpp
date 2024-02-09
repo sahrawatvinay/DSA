c6ea42b1d9c55cde0eb4f37ae5c01b2a089b6acd
vinay <vinaysahrawat183a@gmail.com>
2024-02-09 11:55:22 -0500

    largest subarray with equal no. of 0 and 1

---
+++ Hashing/longest-subarray-with-equal-zeroes-and-ones.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+void longestSubArrayWithEqualZeroesAndOnes(vector<int> &v)
+{
+    for (int i = 0; i < v.size(); i++)
+    {
+        if (v[i] == 0)
+            v[i] = -1;
+    }
+    int maxLen = 0;
+    int prefixSum = 0;
+    unordered_map<int, int> m;
+    for (int i = 0; i < v.size(); i++)
+    {
+        prefixSum += v[i];
+        if (prefixSum == 0)
+            maxLen = i + 1;
+        if (m.find(prefixSum) == m.end())
+            m.insert({prefixSum, i});
+        if (m.find(prefixSum) != m.end())
+            maxLen = max(maxLen, i - m[prefixSum]);
+    }
+    cout << maxLen << endl;
+}
+int main()
+{
+    vector<int> v{1, 0, 1, 1, 1, 0, 0};
+    longestSubArrayWithEqualZeroesAndOnes(v);
+    return 0;
+}
