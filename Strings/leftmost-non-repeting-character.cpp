13509e2ccc467e33d76bb163ae13e84b708f8c30
vinay <vinaysahrawat183a@gmail.com>
2024-03-04 17:49:45 -0500

    minor fix in leftmostnonrepeating char in string

--- Strings/leftmost-non-repeting-character.cpp
+++ Strings/leftmost-non-repeting-character.cpp
@@ -6,7 +6,7 @@ int main()
 {
     const int CHAR = 256;
     vector<int> firstIndex(CHAR, -1); // init char based index vector with -1
-    string str = "abbcbda";
+    string str = "zbbcbda";
     for (int i = 0; i < str.size(); i++) // traverse from left to right
     {
         if (firstIndex[str[i]] == -1) // means 1st occurance, store it's index at that posi
@@ -17,7 +17,7 @@ int main()
     int minIdx = INT_MAX;
     for (int i = 0; i < CHAR; i++)
     {
-        if (firstIndex[i] > 0)                   // these +ve's means they are not repeting and has their indexes stored
+        if (firstIndex[i] >= 0)                  // these +ve's means they are not repeting and has their indexes stored
             minIdx = min(minIdx, firstIndex[i]); // traverse 256 times through ascii values to find the min index stored in these pos values so as to find which is left most non repeting char
     }
     if (minIdx == INT_MAX)
