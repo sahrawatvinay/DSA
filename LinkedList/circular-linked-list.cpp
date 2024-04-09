cc609167cdd05d87f9d33d3068f8cf47df3ba9b6
vinay <vinaysahrawat183a@gmail.com>
2024-04-08 20:41:28 -0400

    circular linked list implementation

---
+++ LinkedList/circular-linked-list.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+void printList(Node *head)
+{
+    if (head == NULL)
+        return;
+    Node *p = head;
+    do
+    {
+        cout << p->data << " ";
+        p = p->next;
+    } while (p != head);
+}
+int main()
+{
+    Node *head = new Node(10);
+    head->next = new Node(20);
+    head->next->next = new Node(30);
+    head->next->next->next = new Node(40);
+    head->next->next->next->next = head; // makes circular and points the last node back to 1st node
+    printList(head);
+    return 0;
+}
