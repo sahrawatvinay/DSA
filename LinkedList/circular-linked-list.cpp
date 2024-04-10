5e911bc31277078f11b9eeab7b4a85c1a6d81216
vinay <vinaysahrawat183a@gmail.com>
2024-04-10 19:46:40 -0400

    circular linked list, insert at beginning

--- LinkedList/circular-linked-list.cpp
+++ LinkedList/circular-linked-list.cpp
@@ -21,6 +21,29 @@ void printList(Node *head)
         p = p->next;
     } while (p != head);
 }
+/// @brief Logic : insert a node bw first and second node
+/// Swap the data of head and head's next, which is new node
+/// 5 will become the first node and 10 will become the second node
+/// @param head
+/// @param x
+Node *insertInBeginning(Node *head, int x)
+{
+    Node *temp = new Node(x);
+    if (head == NULL)
+    {
+        temp->next = temp;
+        return temp;
+    }
+    else
+    {
+        temp->next = head->next;
+        head->next = temp;
+        int t = head->data;
+        head->data = temp->data;
+        temp->data = t;
+        return head;
+    }
+}
 int main()
 {
     Node *head = new Node(10);
@@ -28,6 +51,7 @@ int main()
     head->next->next = new Node(30);
     head->next->next->next = new Node(40);
     head->next->next->next->next = head; // makes circular and points the last node back to 1st node
+    head = insertInBeginning(head, 5);
     printList(head);
     return 0;
 }
