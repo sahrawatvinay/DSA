79785459e46f8508d1dcf6db9920110b3b800d06
vinay <vinaysahrawat183a@gmail.com>
2024-05-08 19:15:30 -0400

    minute changes queue stl, stock-span problem : stack

--- STL/queue.cpp
+++ STL/queue.cpp
@@ -1,6 +1,8 @@
 /*
 - Works on FIFO(First In First Out) principle
-- push, pop, front, empty are main functions
+- push, pop, front, back, empty, size are main functions
+- item inserted at the first will go out when popped // in academics we call pop = dequeue
+- item will always be inserted at the end when we use push // in academics we call push = enqueue
 */
 
 #include <iostream>
@@ -18,5 +20,12 @@ int main()
     cout << "Queue is empty -> " << q.empty() << endl;
     q.pop();
     cout << "First element -> " << q.front() << endl;
+    cout << "Last element -> " << q.back() << endl;
+    //traversal
+    while (!q.empty())
+    {
+        cout << q.front() << "->";
+        q.pop();
+    }
     return 0;
 }
--- Stack/stock-span.cpp
+++ Stack/stock-span.cpp
@@ -2,15 +2,18 @@
 #include <vector>
 #include <stack>
 using namespace std;
+/// @brief find out prices of stocks on n consecutive days
+/// no. of consecutive days including current day and days just before it which has equal or smaller value
+/// @param v stock prices on n consecutive days
 void findStockSpan(vector<int> &v)
 {
     stack<int> st;
-    for (int i = 0; i < v.size(); i++) // traversing through remaining elements
+    for (int i = 0; i < v.size(); i++) // traversing through elements
     {
         while (!st.empty() && v[st.top()] <= v[i]) // keep removing the smaller elements which are in stack as they are of no use
             st.pop();
         int span = st.empty() ? i + 1 : i - st.top(); // compute span, if stack is empty, this means that there is no greater element available in stack that is greater than current element so, store i+1 in stack
-                                                      // otherwise compute span  = index of current element - index of closest greater element on left
+                                                      // otherwise compute span = index of current element - index of closest greater element on left
         cout << span << " ";                          // print span
         st.push(i);                                   // push the current element in stack
     }
